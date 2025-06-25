9aa9f80c0b272b352320521a735bccd3324e536f
Justin Bera <justin.bera@datadoghq.com>
2025-06-25 14:11:56 +0200

    [local] feat(assets): nodeless gpu tolerations in runtimeclass

--- controllers/object_controls.go
+++ controllers/object_controls.go
@@ -4934,6 +4934,7 @@ func transformRuntimeClass(n ClusterPolicyController, spec nodev1.RuntimeClass) 
 		obj.Handler = runtimeClassName
 	}
 
+	obj.Scheduling = spec.Scheduling
 	obj.Labels = spec.Labels
 
 	logger := n.logger.WithValues("RuntimeClass", obj.Name)
