6f94025a82b0b1fd6a4b05f5f4b7c5f8ae081849
Justin Bera <justin.bera@datadoghq.com>
2025-09-25 14:48:18 +0200

    [local] feat(assets): helm keep resource-policy

--- controllers/object_controls.go
+++ controllers/object_controls.go
@@ -4936,6 +4936,7 @@ func transformRuntimeClass(n ClusterPolicyController, spec nodev1.RuntimeClass) 
 
 	obj.Scheduling = spec.Scheduling
 	obj.Labels = spec.Labels
+	obj.SetAnnotations(spec.Annotations)
 
 	logger := n.logger.WithValues("RuntimeClass", obj.Name)
 
